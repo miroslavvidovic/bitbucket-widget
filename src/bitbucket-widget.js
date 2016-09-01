5fd077cfe9197c928f042d334d8719289fffae4f
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-09-01 14:24:19 +0200

    Bitbucket API GET request

---
+++ src/bitbucket-widget.js
@@ -0,0 +1,120 @@
+/** Get the Bitbucket user data
+ * @param {string} username - Bitbucket user username
+ */
+function getUser(username, callback){
+    var xhttpRequest;
+    var bitbucketApi = 'https://api.bitbucket.org/2.0/users/' + username;
+    if (window.XMLHttpRequest) {
+        xhttpRequest = new XMLHttpRequest();
+        } else {
+        // code for IE6, IE5
+        xhttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhttpRequest.open('GET', bitbucketApi, true);
+
+    xhttpRequest.responseType = 'text';
+
+    var response;
+    xhttpRequest.onload = function () {
+        if (xhttpRequest.readyState === xhttpRequest.DONE) {
+            if (xhttpRequest.status === 200) {
+                response = xhttpRequest.responseText;
+                callback(returnData(response));
+            }
+        }
+    };
+
+    xhttpRequest.send(null);
+}
+
+/** Get the information about user repostiories
+ * @param {string} username - Bitbucket user username
+ */
+function getRepositories(username, callback){
+    var xhttpRequest;
+    var bitbucketApi = 'https://api.bitbucket.org/2.0/repositories/' + username;
+    if (window.XMLHttpRequest) {
+        xhttpRequest = new XMLHttpRequest();
+        } else {
+        // code for IE6, IE5
+        xhttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhttpRequest.open('GET', bitbucketApi, true);
+
+    xhttpRequest.responseType = 'text';
+
+    var response;
+    xhttpRequest.onload = function () {
+        if (xhttpRequest.readyState === xhttpRequest.DONE) {
+            if (xhttpRequest.status === 200) {
+                response = xhttpRequest.responseText;
+            }
+
+            callback(returnData(response));
+        }
+    };
+
+    xhttpRequest.send(null);
+}
+
+function returnData(data){
+    var JsonData = JSON.parse(data);
+    return JsonData;
+}
+
+// User username
+var username = 'vidovicmiroslav';
+
+// User avatar
+var avatar = 'https://bitbucket.org/account/'+username+'/avatar/128/';
+
+// Repositories data
+var repositories = getRepositories(username, function(response) {
+    // handleData(response);
+});
+
+// User details
+// var user = getUser(username, function(response){
+//     var created_on = response.created_on;
+//     console.log(response);
+// });
+
+function loadData() {
+
+    // load user data
+    $("#targetUser").load("template.html #user", function() {
+        var template = $("#user").html();
+
+        getUser(username, function(response){
+            var data = {
+             username: username,
+             avatar : avatar,
+             created_on : response.created_on,
+            };
+
+            console.log(response);
+            var rendered = Mustache.render(template, data);
+            $('#targetUser').html(rendered);
+
+        });
+
+    });
+
+    //load repository data
+    $("#targetRepositories").load("repositoryTemplate.html #repositories", function() {
+        var template = $("#repositories").html();
+
+        getRepositories(username, function(response) {
+            var data = {
+                repository : response.values,
+            };
+
+            var rendered = Mustache.render(template, data);
+            $('#targetRepositories').html(rendered);
+
+        });
+
+    });
+}
